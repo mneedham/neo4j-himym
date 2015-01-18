6da8c076e291006991abc1ec2243e316c9836572
Mark Needham <mark.needham@neotechnology.com>
2015-01-18 17:20:05 +0000

    properly named

--- extract_phrases.py
+++ extract_phrases.py
@@ -8,7 +8,10 @@ non_speaker = re.compile('[A-Za-z]+: (.*)')
 
 def untokenize(ngram):
     tokens = list(ngram)
-    return "".join([" "+i if not i.startswith("'") and i not in string.punctuation and i != "n't" else i for i in tokens]).strip()
+    return "".join([" "+i if not i.startswith("'") and \
+                             i not in string.punctuation and \
+                             i != "n't"
+                          else i for i in tokens]).strip()
 
 def extract_phrases(text, phrase_counter, length):
     for sent in nltk.sent_tokenize(text):
@@ -29,8 +32,9 @@ with open("data/import/sentences.csv", "r") as sentencesfile:
     for sentence in reader:
         extract_phrases(sentence[4], phrase_counter, 3)
         count = count + 1
-        # if count > 5:
+        # if count > 1000:
         #     break
 
 most_common_phrases = phrase_counter.most_common(500)
-print most_common_phrases
+for k,v in most_common_phrases:
+    print '{0: <5}'.format(v), k
