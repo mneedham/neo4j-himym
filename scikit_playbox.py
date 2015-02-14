36912d68078ded95a8b39faddd1ad5537b155c06
Mark Needham <mark.needham@neotechnology.com>
2015-02-14 17:24:35 +0000

    pull out old sci-kit code

--- scikit_playbox.py
+++ scikit_playbox.py
@@ -4,6 +4,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from collections import defaultdict
 import csv
 
+
 episodes = defaultdict(list)
 with open("data/import/sentences.csv", "r") as sentences_file:
     reader = csv.reader(sentences_file, delimiter=',')
@@ -20,21 +21,8 @@ corpus = []
 for id, episode in sorted(episodes.iteritems(), key=lambda t: int(t[0])):
     corpus.append(episode)
 
-tf = TfidfVectorizer(analyzer='word', ngram_range=(1,3), min_df = 0, stop_words = 'english')
-tfidf_matrix =  tf.fit_transform(corpus)
-feature_names = tf.get_feature_names()
+td_matrix =  cv.fit_transform(corpus)
 
-with open("data/import/tfidf_scikit.csv", "w") as file:
-    writer = csv.writer(file, delimiter=",")
-    writer.writerow(["EpisodeId", "Phrase", "Score"])
 
-    doc_id = 0
-    for doc in tfidf_matrix.todense():
-        print "Document %d" %(doc_id)
-        word_id = 0
-        for score in doc.tolist()[0]:
-            if score > 0:
-                word = feature_names[word_id]
-                writer.writerow([doc_id+1, word.encode("utf-8"), score])
-            word_id +=1
-        doc_id +=1
+tf_transformer = TfidfTransformer(use_idf=False).fit(td_matrix)
+td_tfidf = tf_transformer.transform(td_matrix)
