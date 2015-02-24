af1d4b37b6e5b94bb6d4eb41f39eeefe21db9e8e
Mark Needham <mark.needham@neotechnology.com>
2015-02-24 22:31:29 +0000

    updating

--- scripts/detect_speaker.py
+++ scripts/detect_speaker.py
@@ -1,12 +1,27 @@
 import nltk
 import json
-import pickle
 import collections
 
 from sklearn.cross_validation import train_test_split
 from himymutil.ml import pos_features
 from himymutil.naive import NaiveClassifier
 from tabulate import tabulate
+
+def assess_classifier(classifier, test_data, text):
+    refsets = collections.defaultdict(set)
+    testsets = collections.defaultdict(set)
+    for i, (feats, label) in enumerate(test_data):
+        refsets[label].add(i)
+        observed = classifier.classify(feats)
+        testsets[observed].add(i)
+
+    speaker_precision = nltk.metrics.precision(refsets[True], testsets[True])
+    speaker_recall = nltk.metrics.recall(refsets[True], testsets[True])
+
+    non_speaker_precision = nltk.metrics.precision(refsets[False], testsets[False])
+    non_speaker_recall = nltk.metrics.recall(refsets[False], testsets[False])
+
+    return [text, speaker_precision, speaker_recall, non_speaker_precision, non_speaker_recall]
 
 with open("data/import/trained_sentences.json", "r") as json_file:
     json_data = json.load(json_file)
@@ -23,34 +38,9 @@ for tagged_sent in tagged_sents:
 
 train_data,test_data = train_test_split(featuresets, test_size=0.20, train_size=0.80)
 
-def assess_classifier(classifier, test_data, text):
-    refsets = collections.defaultdict(set)
-    testsets = collections.defaultdict(set)
-    for i, (feats, label) in enumerate(test_data):
-        refsets[label].add(i)
-        observed = classifier.classify(feats)
-        testsets[observed].add(i)
-
-    speaker_precision = nltk.metrics.precision(refsets[True], testsets[True])
-    speaker_recall = nltk.metrics.recall(refsets[True], testsets[True])
-
-    non_speaker_precision = nltk.metrics.precision(refsets[False], testsets[False])
-    non_speaker_recall = nltk.metrics.recall(refsets[False], testsets[False])
-
-    row = [text, speaker_precision, speaker_recall, non_speaker_precision, non_speaker_recall]
-
-    with open("classifiers/" + text.lower().replace(" ", "_") + ".pickle", "w") as f:
-        pickle.dump(classifier, f)
-    return row
-
 table = []
 table.append(assess_classifier(NaiveClassifier(), test_data, "Naive"))
 table.append(assess_classifier(nltk.NaiveBayesClassifier.train(train_data), test_data, "Naive Bayes"))
 table.append(assess_classifier(nltk.DecisionTreeClassifier.train(train_data), test_data, "Decision Tree"))
 
 print(tabulate(table, headers=["Classifier","speaker precision", "speaker recall", "non-speaker precision", "non-speaker recall"]))
-
-# sentence = "Ted from 2030: Oh,we were"
-# tokenized_sentence = nltk.word_tokenize(sentence)
-# for i, word in enumerate(tokenized_sentence):
-#     print "{0} -> {1}".format(word, classifier.classify(pos_features(tokenized_sentence, i)))
